60b886d12f8860b1b1cf83e1af9a1de2870ada36
TimRas <martinusras95@gmail.com>
2022-09-29 17:50:57 +0000

    Made toasts work

--- bag/views.py
+++ bag/views.py
@@ -5,12 +5,10 @@ from products.models import Product
 
 # Create your views here.
 
-
 def view_bag(request):
-    """ A view that renders the bag contents page """ 
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
-
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -40,10 +38,10 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-
+    
 
 def adjust_bag(request, item_id):
-    """ Adjust the quantity of the specified product to the specified amount """
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -56,12 +54,12 @@ def adjust_bag(request, item_id):
             bag[item_id]['items_by_size'][size] = quantity
         else:
             del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size'][size]:
+            if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
     else:
         if quantity > 0:
             bag[item_id] = quantity
-        else: 
+        else:
             bag.pop(item_id)
 
     request.session['bag'] = bag
@@ -69,7 +67,8 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """ Remove the item from the shopping bag """
+    """Remove the item from the shopping bag"""
+
     try:
         size = None
         if 'product_size' in request.POST:
@@ -78,14 +77,13 @@ def remove_from_bag(request, item_id):
 
         if size:
             del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size'][size]:
+            if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
         else:
             bag.pop(item_id)
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
+
     except Exception as e:
         return HttpResponse(status=500)
-
-
